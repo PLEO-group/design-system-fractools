ec2596abb781ba14fa5068b7d7cbe9e13f06f5a3
Szymon <s.stawowy@pleodigital.com>
2026-06-19 11:25:46 +0200

    basic style dictionary config

---
+++ dist/js/tailwind-preset.js
@@ -0,0 +1,18 @@
+/**
+ * Ta paczka jest generowana automatycznie przez Style Dictionary.
+ * NIE edytuj jej ręcznie.
+ */
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+  "brand-primary": "#22d3ee",
+  "brand-secondary": "#8b5cf6",
+  "brand-highlight": "#b8b0d8"
+},
+      spacing: {},
+      borderRadius: {}
+    }
+  },
+  plugins: []
+};
